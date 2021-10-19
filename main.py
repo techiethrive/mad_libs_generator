df346b1b4769991b180610b0989c9e96ac8cb4c0
Aman Kumar <80822318+techiethrive@users.noreply.github.com>
2021-10-19 21:44:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+""" Mad Libs Generator
+----------------------------------------
+"""
+//Loop back to this point once code finishes
+loop = 1
+while (loop < 10):
+// All the questions that the program asks the user
+    noun = input("Choose a noun: ")
+    p_nou = input("Choose a noun: ")
+// Displays the story based on the users input
+    print ("------------------------------------------")
+    print ("Be kind to your",noun,"- footed", p_noun)
+    print ("For a duck 
+    print ("------------------------------------------")
+// Loop back to "loop = 1"
+    loop = loop + 1
